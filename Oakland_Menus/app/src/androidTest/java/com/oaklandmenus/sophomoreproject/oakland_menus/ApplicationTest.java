df900ab6d4c8249f4658856d74d4a78826e498bb
brian31988 <brian31988@gmail.com>
2015-10-27 11:13:40 -0400

    uploading main project
    uploading main project

---
+++ Oakland_Menus/app/src/androidTest/java/com/oaklandmenus/sophomoreproject/oakland_menus/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.oaklandmenus.sophomoreproject.oakland_menus;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
